e13b0bdcf1f54a1fb557bfe7ebd2d282f15215b7
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-22 16:35:17 +0000

    Initial commit

---
+++ app/src/main/java/example/codeclan/com/wordcounter/WordCounter.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.wordcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_counter);
+    }
+}
